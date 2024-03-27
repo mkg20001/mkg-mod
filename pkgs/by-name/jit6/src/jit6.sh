33e582a96c9174181684e703633a84607848807f
Maciej Krüger <mkg20001@gmail.com>
2024-03-27 18:15:16 +0100

    Jit6 use config

--- pkgs/by-name/jit6/src/jit6.sh
+++ pkgs/by-name/jit6/src/jit6.sh
@@ -2,7 +2,9 @@
 
 export PATH="@path@"
 
-PREF="2a01:7e01:e003:2864"
+. /etc/jit6
+
+PREF="$IPV6_64_PREFIX"
 PREFSIZE="64";
 SUF=$(hexdump -vn8 -e'2/1 "%1x" 1 ":"' /dev/urandom | sed 's|:$||')
 
@@ -26,7 +28,7 @@ DNS = 2606:4700:4700::1111, 2606:4700:4700::1001, 1.1.1.1, 1.0.0.1
 PublicKey = DS8+lOzNk9IAyuQ9W0ABENnwowSo/gluaeuBTwPmO20=
 
 # Public IP address of your VPN server
-Endpoint = 172.104.147.111:6464
+Endpoint = $INCOMING_IP:6464
 
 # VPN Subnet and IPv6 Tunnel
 AllowedIPs = ::/0
